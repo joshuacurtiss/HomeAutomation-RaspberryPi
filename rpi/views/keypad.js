565cc1ce98d8e89f691e218d15e7928d5c7e6479
Josh Curtiss <josh@curtiss.me>
2016-12-15 15:50:12 -0600

    Updated keypad to use configs.
    It can now change how many digits its looking for based on configs.
    
    Also can store multiple passcodes, and distinguish who they are for and whether they are duress codes.
    
    Also, now displays an alarm image for the keypad screen.

--- rpi/views/keypad.js
+++ rpi/views/keypad.js
@@ -1,30 +1,52 @@
 let jQuery=$=require("jquery");
+let data=require("../data/data.json");
 $(document).ready(function(){
+    // Draw the passcode dots
+    var passcodeLength=data.passcodeLength || 4;
+    for( var i=0 ; i<passcodeLength ; i++ ) $(".passcodeui").append('<i class="fa fa-circle-thin"></i>');
+    // Wire up keypad handlers
+    $(window).keypress(handleKeypress);
     $(".keypad button").click(handleKeypadClick);
     $(".keypad").submit(handleKeypadSubmit);
 });
 function handleKeypadClick(){
-    var $passcodedots=$(".passcodeui i");
-    // Determine the new value of the passcode based on button pressed
-    var passcode=$("#passcode").val();
-    if( this.value=="clear" ) {
-        passcode="";
-    } else if( this.value=="delete" ) {
-        passcode=passcode.slice(0,-1);
-    } else if( passcode.length<$passcodedots.length ) {
-        passcode+=this.value;
-    }
-    $("#passcode").val(passcode);
-    // Update passcode ui
-    $passcodedots.removeClass("fa-circle-thin fa-circle");
-    $passcodedots.each(function(key,obj){
-        $(obj).addClass( (key<passcode.length)?"fa-circle":"fa-circle-thin" );
-    });
-    // Submit if passcode is full
-    if( passcode.length==$passcodedots.length ) $(this).parent("form").submit();
+    addPasscodeDigit(this.value);
+}
+function handleKeypress(event){
+    addPasscodeDigit(event.key);
 }
 function handleKeypadSubmit(){
     var passcode=$("#passcode").val();
-    setTimeout(function(){alert(passcode)},200);
+    var codedata=data.passcodes[passcode];
+    if( codedata ) {
+        alert(`Successful ${codedata.type} code named "${codedata.name}".`);
+    } else {
+        alert("NOPE!");
+        updatePasscode("");
+    }
     return false;
+}
+function addPasscodeDigit(digit){
+    var $passcodedots=$(".passcodeui i");
+    // Determine the new value of the passcode based on button pressed
+    var passcode=$("#passcode").val();
+    if( digit=="clear" ) {
+        passcode="";
+    } else if( digit=="delete" ) {
+        passcode=passcode.slice(0,-1);
+    } else if( passcode.length<$passcodedots.length && digit>="0" && digit<="9" ) {
+        passcode+=digit;
+    }
+    // Update passcode
+    updatePasscode(passcode);
+    // Submit if passcode is full
+    if( passcode.length==$passcodedots.length ) $(".keypad").submit();
+}
+function updatePasscode(passcode){
+    $("#passcode").val(passcode);
+    $(".passcodeui i")
+        .removeClass("fa-circle-thin fa-circle")
+        .each(function(key,obj){
+            $(obj).addClass( (key<passcode.length)?"fa-circle":"fa-circle-thin" );
+        });
 }

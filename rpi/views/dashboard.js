d2fe52206eba650901be947b08e0c514b8cc2f13
Josh Curtiss <git@curtiss.me>
2016-12-15 02:07:17 -0600

    Preliminary dashboard work. Still not actually functioning, but it's starting to come together. Widget types are defined in configs, and widget specs are defined in a JSON data file.
    Removed the usage of Bootstrap for now. I may use some other engine for doing effects.

--- rpi/views/dashboard.js
+++ rpi/views/dashboard.js
@@ -1,6 +1,53 @@
 let jQuery=$=require("jquery");
-require("bootstrap");
+let config = require("config");
+let data=require("../data/data.json");
+let widgetTypes=config.get("widgetTypes");
+let moment=require("moment");
+
+const TIMEFMT="h:mm A";
+const DOWFMT="dddd";
+const DATEFMT="MMMM D";
 
 $(document).ready(()=>{
-    $("#msg").html("Hello world!");
+    var html="", widgetType={};
+    loadStylesheet(`../css/dashboard-${data.theme}.css`);
+    for( var widget of data.widgets )
+    {
+        widgetType=widgetTypes[widget.type];
+        if( widgetType )
+        {
+            if( widget.type=="datetime" )
+            {
+                html=`<div data-type="${widget.type}" class="widget ${widgetType.classes || ''}">` +
+                     `<h1 class="date"></h1>` +
+                     `<div class="timedow"></div>`+
+                     `<div class="time"></div>`+
+                     `</div>`;
+            }
+            else
+            {
+                html=`<div data-type="${widget.type}" class="widget ${widgetType.classes || ''}">` +
+                     `<h1>${widget.title || ''}</h1>` +
+                     `<i class="${widgetType.iconoff}"></i>`;
+                if( widget.battery ) html+=`<span class="fa fa-battery-${widget.battery}"></span>`;
+                html+=`</div>`;
+            }
+            $(".dashboard").append(html);
+        }
+    }
+    updateTime();
 });
+
+function updateTime()
+{
+    var now=moment();
+    $(".date").html(now.format(DATEFMT));
+    $(".time").html(now.format(TIMEFMT));
+    $(".timedow").html(now.format(DOWFMT));
+    var sec=now.seconds();
+    setTimeout(updateTime,(60-sec)*1000);
+}
+
+function loadStylesheet(path) {
+    $("head").append(`<link rel='stylesheet' href='${path}' />`);
+}
